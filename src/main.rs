189fc1dccb48c9267c10863d8c0b5ba94f4f17e1
Ollie <75216562+ZJAC281@users.noreply.github.com>
2024-12-19 14:00:55 +0000

    Added agument based file opening

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let path = &args[1];
+
+    let contents = match fs::read_to_string(path){
+        Ok(result) => result,
+        Err(e) => panic!("Error: {e}"),
+    };
+    println!("contents: {contents}");
+
 }
