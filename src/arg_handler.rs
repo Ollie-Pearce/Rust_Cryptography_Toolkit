60630fc68bbb5c56ef44ac85a240d01220974047
Ollie <75216562+ZJAC281@users.noreply.github.com>
2024-12-20 19:46:52 +0000

    Added argument handling for RSA

--- src/arg_handler.rs
+++ src/arg_handler.rs
@@ -1,6 +1,7 @@
 pub struct ConfigStruct {
     pub caesar_shift: Option<u8>,
     pub vignere_key: Option<String>,
+    pub rsa_key: Option<(i64, i64)>,
     pub file_path: String,
 }
 
@@ -15,6 +16,7 @@ pub fn parse_args(mut args: Vec<String>) -> Result<ConfigStruct, String> {
 
     let mut caesar_shift: Option<u8> = None;
     let mut vignere_key: Option<String> = None;
+    let mut rsa_key = None;
 
     let mut i = 1;
     let mut iter = args[1..].iter();
@@ -33,9 +35,19 @@ pub fn parse_args(mut args: Vec<String>) -> Result<ConfigStruct, String> {
                 vignere_key = Some(key.to_string());
             }
 
+            "-rsa" => {
+                let key_1_str = iter.next().ok_or_else(||"Missing RSA encryption key")?;
+
+                let key_2_str = iter.next().ok_or_else(||"Missing second RSA key")?;
+
+                let key_1 = key_1_str.parse::<i64>().map_err(|_|"Error parsing RSA key 1 into integer")?;
+                let key_2 = key_2_str.parse::<i64>().map_err(|_|"Error parsing RSA key 2 into integer")?;
+                
+                rsa_key = Some((key_1, key_2));
+            }
+
             _ => {
-                {
-                }
+                {}
             }
         }
     }
@@ -43,6 +55,7 @@ pub fn parse_args(mut args: Vec<String>) -> Result<ConfigStruct, String> {
     Ok(ConfigStruct {
         caesar_shift,
         vignere_key,
+        rsa_key,
         file_path,
     })
 }
